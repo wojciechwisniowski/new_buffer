dbc39d2cb56af8c54f81139732856124c67c5252
wojciech.wisniowski <wojciech.wisniowski@comarch.com>
2016-12-26 12:46:47 +0100

    floor pump handling v1.8

--- driver_2015/driver_net.cpp
+++ driver_2015/driver_net.cpp
@@ -56,7 +56,7 @@ void setupHttp() {
 //from webServerExample
 
 void printHtmlBufor(EthernetClient& client) {
-	client.println(F("\"VER\":1.7"));
+	client.println(F("\"VER\":1.8"));
 	for (int i = 0; i < TEMPCOUNT; i++) {
 
 		float tempC = getTempC(i);
